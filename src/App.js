9c5ed6fc70521ae82bbdfc3da54ac270bb97644d
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-10-25 09:26:14 +0200

    Launch screen

--- src/App.js
+++ src/App.js
@@ -32,69 +32,25 @@ function App() {
         align='center'
         justify='center'
         wrap='nowrap'
+        bg='black'
       >
         <Helmet>
-          <title>Signap Group GMBH ©®</title>
+          <title>Going public ©®</title>
         </Helmet>
 
-        <SwipeableViews
-          index={ index }
-          onChangeIndex={( value )=> {setIndex( value )}}
-        >
-          <Box>
-            <Team transition={true} />
-          </Box>
-          <Box>
-            <Home
-              prev={()=>{ setIndex(0) }}
-              next={()=>{ setIndex(2) }}
-              transition={true} />
-          </Box>
-          <Box>
-            <Projects index={1} />
-          </Box>
+        
 
-        </SwipeableViews>
+       <Box
+        as='video'
+        autoPlay
+        loop
+        muted
+        src='./going-public-launch.mp4'
 
-
-        {/* { components[component] === 'Home' ?
-            <Home transition={true} />
-            : components[component] === 'Team'
-              ? <Team transition={true} />
-                : components[component] === 'Projects'
-                  ? <Projects transition={true} />
-                    : null
-        } */}
-
-        {/* <Box>
-          Swipe : { components[component] }
-          <HStack spacing={ 10 }>
-            <Button>Projects</Button>
-            <Button>Team</Button>
-          </HStack>
-        </Box> */}
-      </Flex>
-      <Flex
-        as='footer'
-        bottom='0'
-        p={ 10 }
-        position='fixed'
         w='100vw'
-        justify='space-between'
-        display={{ base: 'none', lg:'flex'}}
+        h='100vh'
+      />
 
-      >
-        <Button
-          visibility={index === 0 ? 'hidden' : 'initial'}
-          colorScheme="red" onClick={()=>{setIndex(index - 1) }}>
-            { index === 1 ? 'Équipe' : 'Home' }
-        </Button>
-        <Button
-          visibility={index === 2 ? 'hidden' : 'initial'}
-          colorScheme="red" onClick={()=>{setIndex(index + 1) }}>
-                        { index === 1 ? 'Projets' : 'Home' }
-
-        </Button>
       </Flex>
     </ChakraProvider>
   );
